ca2d4785013abe18f2fd70e217d2516a8cbdc0a7
Oksana Goncharova <oksanagoncharova2011@gmail.com>
2026-08-18 17:36:50 +0800

    Added output with two languages

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Привет мир yang!"));
-
+    IO.println(String.format("Привет мир!"));
+    IO.println(String.format("Hello word"));
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
